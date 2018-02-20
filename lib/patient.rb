4fa0cca063ea366a8c43af42a2e23f0a89061c6b
Kimberly Huynh and Ron Craig <ron@>
2018-02-20 11:28:33 -0800

    3 specs pass. override duplicates

--- lib/patient.rb
+++ lib/patient.rb
@@ -8,8 +8,25 @@ class Patient
     @dob = attributes.fetch(:dob)
   end
 
-  def self.all()
-    patient_list = []
+  def self.all
+      returned_patients = DB.exec("SELECT * FROM patients;")
+      patient_lists = []
+      returned_patients.each() do |patient|
+        name = patient.fetch("name")
+        dob = patient.fetch("dob")
+        id = patient.fetch("id").to_i()
+        patient_lists.push(Patient.new({:name => name, :dob => dob}))
+      end
+      patient_lists
+  end
+
+
+  def save
+    DB.exec("INSERT INTO patients (name, dob) VALUES ('#{@name}', '#{@dob}') RETURNING id;")
+  end
+  #
+  def ==(other_patient)
+    self.name().==(other_patient.name())
   end
   
 end
--- spec/patient_spec.rb
+++ spec/patient_spec.rb
@@ -1,14 +1,20 @@
 require('rspec')
-require('patient')
 require('pg')
+require('patient')
+
 
 DB = PG.connect({:dbname => 'doctors_office_test'})
 
+RSpec.configure do |config|
+  config.after(:each) do
+    DB.exec("DELETE FROM patients *;")
+  end
+end
 
 describe(Patient) do
 
   describe(".all") do
-      it("starts off with no patient") do
+      it("starts off with an empty array") do
         expect(Patient.all()).to(eq([]))
       end
     end
@@ -21,4 +27,16 @@ describe(Patient) do
       end
     end
 
-end
+  # describe(".all") do
+  #   it("adds a patient to the array of saved patients")
+  #     patient1 = Patient.new({:name => "Joey", :dob => "2008-01-01 00:00:00"})
+
+  describe("#==") do
+    it("same patient with the same name") do
+      patient1 = Patient.new({:name => "Aria", :dob => "1999-03-08 00:00:00"})
+      patient2 = Patient.new({:name => "Aria", :dob => "1999-03-08 00:00:00"})
+      expect(patient1).to(eq(patient2))
+    end
+  end
+
+  end
