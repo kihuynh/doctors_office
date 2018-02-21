dace9670ae1cf52fb4c7f33cc858e489e06f70a5
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-02-21 15:43:21 -0800

    backup database. Begin on home, doctor views. With accompanying routes

--- app.rb
+++ app.rb
@@ -1,17 +1,23 @@
-# require('sinatra')
-# require('sinatra/reloader')
-# also_reload('lib/**.*.rb')
-# require('./lib/doctor')
-# require('./lib/patient')
-# require('pg')
+require('sinatra')
+require('sinatra/reloader')
+also_reload('lib/**.*.rb')
+require('./lib/doctor')
+require('./lib/patient')
+require('pg')
+
+DB = PG.connect({:dbname => "doctors_office"})
+# restful something. took photos.
+get('/') do
+  # Display home page
+  erb(:home)
+end
 #
-# DB = PG.connect({:dbname => "doctors_office"})
-# # restful something. took photos.
-# get('/') do
-#   # Display home page
-#   erb(:home)
-# end
-#
-# get('/output') do
-#   erb(:output)
-# end
+get('/doctors') do
+# Display current doctor info
+  erb(:doctors)
+end
+
+get('patients') do
+  # Display current Patients
+  erb(:patients)
+end
