33e1203f2a52ac5332e24ac597034b70c03105c8
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-02-21 22:05:24 -0800

    try to debug, and be stuck

--- app.rb
+++ app.rb
@@ -25,29 +25,30 @@ get('/patients') do
   erb(:patients)
 end
 
-# get('/doctors-new') do
-# # new doctor form
-#   erb(:doctors_new)
-# end
-# get('/patients-new') do
-# # new doctor form
-#   erb(:patients_new)
-# end
-#
-# post('/patients') do
-#   # new patient information. params and such to save
-#   name = params.fetch['name']
-#   dob = params.fetch['dob']
-#   patient = Patient.new({:name => name, :dob => dob, :doctor_id => nil})
-#   patient.save()
-#   @patient = Patient.all()
-#   erb(:patients_new)
-# end
+get('/doctors-new') do
+# new doctor form
+  erb(:doctors_new)
+end
+get('/patients-new') do
+# new doctor form
+  erb(:patients_new)
+end
+
+post('/patients') do
+  # new patient information. params and such to save
+  name = params.fetch('name')
+  dob = params.fetch('dob')
+  patient = Patient.new({:name => name, :dob => dob, :doctor_id => nil, :id => nil})
+  patient.save()
+  # Look at RESTFUL lesson for post
+  @patient = Patient.all()
+  erb(:patients_new)
+end
 #
 # post('/doctors_new') do
 # # new doctor information. params and such and saving and stuff
-#   name = params.fetch['name']
-#   specialty = params.fetch['specialty']
+#   name = params.fetch('name')
+#   specialty = params.fetch('specialty')
 #   doc = Doctor.new({:name => name, :specialty => specialty, :id => nil})
 #   doc.save()
 #   @doctors = Doctor.all()
--- lib/patient.rb
+++ lib/patient.rb
@@ -1,28 +1,27 @@
 #!/usr/bin/ruby
 
 class Patient
-  attr_reader(:name, :dob, :doctor_id, :id)
+  attr_reader(:name, :dob, :doctor_id)
   # add :id back into reader to initialize the id in .all
   def initialize(attributes)
     @name = attributes.fetch(:name)
     @dob = attributes.fetch(:dob)
     @doctor_id = attributes.fetch(:doctor_id)
-    @id = attributes.fetch(:id)
   end
 
   def self.all
-      returned_patients = DB.exec('SELECT * FROM patients;')
-      patient_lists = []
-      returned_patients.each() do |patient|
-        # for each patient grab each info
-        name = patient.fetch('name')
-        dob = patient.fetch('dob')
-        id = patient.fetch('id').to_i()
-        doctor_id = patient.fetch('doctor_id').to_i()
-        # push all new patient info into patient list
-        patient_lists.push(Patient.new({:name => name, :dob => dob, :doctor_id => doctor_id, :id => id}))
-      end
-      patient_lists
+    returned_patients = DB.exec('SELECT * FROM patients;')
+    patient_lists = []
+    returned_patients.each() do |patient|
+      # for each patient grab each info
+      name = patient.fetch('name')
+      dob = patient.fetch('dob')
+      id = patient.fetch('id').to_i()
+      doctor_id = patient.fetch('doctor_id').to_i()
+      # push all new patient info into patient list
+      patient_lists.push(Patient.new({:name => name, :dob => dob, :doctor_id => doctor_id})
+    end
+    patient_lists
   end
 
 
